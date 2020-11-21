d99097a79bf3e39b76618e0022a2865bf6cd5f12
Erik Seliger <erikseliger@me.com>
2020-11-21 02:17:19 +0100

    Restore backward compatibility with GitHub enterprise 2.20
    This is highly WIP, don't review.

--- cmd/repo-updater/repos/github.go
+++ cmd/repo-updater/repos/github.go
@@ -114,6 +114,17 @@ func newGithubSource(svc *ExternalService, c *schema.GitHubConnection, cf *httpc
 
 	token := &auth.OAuthBearerToken{Token: c.Token}
 
+	v3Client := github.NewV3Client(apiURL, token, cli)
+	v4Client := github.NewV4Client(apiURL, token, cli)
+
+	if !githubDotCom {
+		enterpriseVersion, err := v3Client.GetEnterpriseVersion(context.Background())
+		if err != nil {
+			return nil, err
+		}
+		v4Client.EnterpriseVersion = enterpriseVersion
+	}
+
 	return &GithubSource{
 		svc:              svc,
 		config:           c,
@@ -122,8 +133,8 @@ func newGithubSource(svc *ExternalService, c *schema.GitHubConnection, cf *httpc
 		excludeForks:     excludeForks,
 		baseURL:          baseURL,
 		githubDotCom:     githubDotCom,
-		v3Client:         github.NewV3Client(apiURL, token, cli),
-		v4Client:         github.NewV4Client(apiURL, token, cli),
+		v3Client:         v3Client,
+		v4Client:         v4Client,
 		searchClient:     github.NewV3Client(apiURL, token, cli).WithSeparateRateLimitMonitor(),
 		originalHostname: originalHostname,
 	}, nil
